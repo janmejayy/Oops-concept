0e975dba99a0d0d34ecb0039c007b80702b45883
Jay <janmejay.yadav.1234@gmail.com>
2022-02-26 15:55:26 +0530

    oops concept

---
+++ OopsConcept/src/AbstractClass/AbstractClassWithMethods.java
@@ -0,0 +1,20 @@
+package AbstractClass;
+
+public class AbstractClassWithMethods {
+
+}
+abstract class Bike{  
+	  abstract void run();  
+	  int a;
+	}
+
+	class Honda4 extends Bike{  
+		void run(){
+			System.out.println("running safely");
+			}  
+	public static void main(String args[]){  
+	 Bike obj = new Honda4();  
+	 obj.run(); 
+	 
+	}  
+	}  
---
+++ OopsConcept/src/Interface/MultipleInheritace.java
@@ -0,0 +1,19 @@
+package Interface;
+
+public class MultipleInheritace {
+
+}
+interface Printable{  
+void print();  
+}  
+interface Showable{  
+void print();  
+}  
+  
+class TestInterface3 implements Printable, Showable{  
+public void print(){System.out.println("Hello");}  
+public static void main(String args[]){  
+TestInterface3 obj = new TestInterface3();  
+obj.print();  
+ }  
+}  
